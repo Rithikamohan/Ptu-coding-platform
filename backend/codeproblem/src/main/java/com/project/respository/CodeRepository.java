50260d79ae59c379082186c4f2ee0260cf34741a
Rithikamohan <122177326+Rithikamohan@users.noreply.github.com>
2023-05-25 12:32:41 +0530

    Delete backend/codeproblem/src/main/java/com/project directory

--- backend/codeproblem/src/main/java/com/project/respository/CodeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.project.respository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.project.model.*;
-public interface CodeRepository extends JpaRepository<Code, Integer> {
-
-}
--- backend/codeproblem/src/main/java/com/project/respository/CplusRepository.java
+++
@@ -1,6 +0,0 @@
-package com.project.respository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.project.model.*;
-public interface CplusRepository extends JpaRepository<Cplus, Integer> {
-
-}
