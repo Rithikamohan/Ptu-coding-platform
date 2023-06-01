28af56f5cc9a6660f607674cf1ca8a89d9a5dbf4
Rithikamohan <122177326+Rithikamohan@users.noreply.github.com>
2023-06-01 11:25:53 +0530

    Add files via upload

---
+++ backend/codeproblem/src/test/java/com/project/CodeproblemsApplicationTests.java
@@ -0,0 +1,18 @@
+package com.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeproblemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+
+	@Test
+	public void main() {
+		CodeproblemsApplication.main(new String[] {});
+	}
+}
