11f23dbe01e8ecef0384cbc55a9ad4217f3f8a55
Rithikamohan <122177326+Rithikamohan@users.noreply.github.com>
2023-06-01 11:23:38 +0530

    Update ApplicationTests.java

--- backend/faculty/src/test/java/com/project/ApplicationTests.java
+++ backend/faculty/src/test/java/com/project/ApplicationTests.java
@@ -19,85 +19,9 @@ public class ApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
-	 @Autowired
-	    private UserRepository employeeRepository;
-Long id;
-
-
-	    @Test
-	    @Order(1)
-	    @Rollback(value = false)
-	    public Long saveEmployeeTest(){
-
-	        User employee = User.builder()
-	        		.name("rithika")
-					.email("Rit@gmail.com")
-					
-					.mobile("1234567891")
-					.password("00000000")
-	                .build();
-	                 
-	        employeeRepository.saveAndFlush(employee);
-	        System.out.println( "Id in save employee"+id);
-	        Assertions.assertThat(employee.getId()).isGreaterThan(0);
-	        Long id=employee.getId();
-	        return null;
-	    
-	    }
+	@Test
+	public void main() {
+	Application.main(new String[] {});
+	}
 	
-	
-//	    @Test
-//	    @Order(2)
-//	    public void getEmployeeTest(){
-//	    	Long id=saveEmployeeTest();
-//	    	System.out.println( "Id in get employee"+id);
-//	        User employee = employeeRepository.findById(id).get();
-//
-//	        Assertions.assertThat(employee.getId()).isEqualTo(id);
-//
-//	    }
-	
-	    @Test
-	    @Order(3)
-	    public void getListOfEmployeesTest(){
-
-	        List<User> employees = employeeRepository.findAll();
-
-	        Assertions.assertThat(employees.size()).isGreaterThan(0);
-
-	    }
-//	
-//	    @Test
-//	    @Order(4)
-//	    @Rollback(value = false)
-//	    public void updateEmployeeTest(){
-//	    	Long id=saveEmployeeTest();
-//	        User employee = employeeRepository.findById(id).get();
-//
-//	        employee.setEmail("ram@gmail.com");
-//
-//	        User employeeUpdated =  employeeRepository.save(employee);
-//
-//	        Assertions.assertThat(employeeUpdated.getEmail()).isEqualTo("ram@gmail.com");
-//
-//	    }
-//	    @Test
-//	    @Order(5)
-//	    @Rollback(value = false)
-//	    public void deleteEmployeeTest(){
-//	    	Long id=saveEmployeeTest();
-//	        User employee = employeeRepository.findById(id).get();
-//
-//	        employeeRepository.delete(employee);
-//	        User employee1 = null;
-//
-//	        Optional<User> optionalEmployee = employeeRepository.findById(id);
-//
-//	        if(optionalEmployee.isPresent()){
-//	            employee1 = optionalEmployee.get();
-//	        }
-//
-//	        Assertions.assertThat(employee1).isNull();
-//	    }
-
 }
